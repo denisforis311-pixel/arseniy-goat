04d151951610ea8f4fa5e00e8b5826a6856cc676
denisforis311-pixel <denisforis311@gmail.com>
2026-06-25 16:19:15 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask,send_from_directory
+from flask import Flask, send_from_directory
 import os
-
 
 app = Flask(__name__)
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def home():
     return "<h1>Arseniy goat!!!</h1><img src='/get-photo' style='max-width:100%;'>"
 
-
 @app.route('/get-photo')
 def get_photo():
-    return send_from_directory(os.getcwd(), 'photo.jpg')
+    return send_from_directory(BASE_DIR, 'photo.jpg')
 
-
-if name == '__main__':
-    port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0',port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
